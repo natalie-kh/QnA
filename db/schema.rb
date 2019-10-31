88cd9ee3875b3606efc754f2f895e6fdf35cd15c
natalya.bogdanova <nata-mini@mail.ru>
2019-10-31 21:21:33 +0300

    Added Answer model, migration and first test

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_120511) do
+ActiveRecord::Schema.define(version: 2019_10_31_181643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
