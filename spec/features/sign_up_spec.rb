0b39b134595b248b777520036257b409d6ba9943
natalya.bogdanova <nata-mini@mail.ru>
2019-11-06 20:49:36 +0300

    Tests for sign_up

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature 'User can sign up', "
+  In order to ask questions
+  As an guest user
+  I'd like to be able to sign up
+" do
+  given(:user) { create(:user) }
+
+  background { visit new_user_registration_path }
+
+  scenario 'New user tries to sign up' do
+    fill_in 'Email', with: 'new@test.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+
+    click_button 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+
+  scenario 'Already existing user tries to sign up' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password
+
+    click_button 'Sign up'
+
+    expect(page).to have_content 'Email has already been taken'
+  end
+end
