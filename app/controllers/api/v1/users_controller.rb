788855af6a1532ccaa001fe2c4a62248490bb361
natalya.bogdanova <nata-mini@mail.ru>
2020-02-15 16:18:21 +0300

    Fixes after review

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,10 +3,10 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   def index
     @users = User.where.not(id: current_resource_owner.id)
-    render json: @users, each_serializer: UserSerializer
+    render json: @users
   end
 
   def me
-    render json: current_resource_owner, serializer: UserSerializer
+    render json: current_resource_owner
   end
 end
