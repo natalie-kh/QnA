015449e17b906510afdce453d3592d4bbf58f583
natalya.bogdanova <nata-mini@mail.ru>
2020-02-29 00:08:06 +0300

    Updated creds

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,8 +68,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
-      user_name:            ENV['SMTP_USERNAME'],
-      password:             ENV['SMTP_PASSWORD'],
+      user_name:            ENV[:smtp][:user_name],
+      password:             ENV[:smtp][:password],
       authentication:       'plain',
       enable_starttls_auto: true }
 
