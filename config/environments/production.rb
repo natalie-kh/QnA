8304686f4f2e4b833885bbf2d429a2c21e56cb8c
natalya.bogdanova <nata-mini@mail.ru>
2020-02-29 00:19:33 +0300

    changed rails env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,8 +68,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
-      user_name:            ENV[:smtp][:user_name],
-      password:             ENV[:smtp][:password],
+      user_name:            Rails.application.credentials[Rails.env.to_sym][:smtp][:user_name],
+      password:             Rails.application.credentials[Rails.env.to_sym][:smtp][:password],
       authentication:       'plain',
       enable_starttls_auto: true }
 
